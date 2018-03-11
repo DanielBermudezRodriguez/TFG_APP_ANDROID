5abc4ef5df02b9f8385e56ce4414ea0f9d19140e
DaniBermudezRodriguez <dani_bermu12@hotmail.com>
2018-03-11 21:53:51 +0100

    Actualizadas y comentada constantes clase Global

--- app/src/main/java/org/udg/pds/todoandroid/util/Global.java
+++ app/src/main/java/org/udg/pds/todoandroid/util/Global.java
@@ -4,9 +4,9 @@ package org.udg.pds.todoandroid.util;
 public class Global {
 
     public static final String BASE_URL = "http://192.168.42.132:8080/rest/";
-    public static final int REQUEST_CODE_GPS_LOCATION = 1000;
-    public static final long MINIMUM_TIME_UPDATE_LOCATION = 60000;
-    public static final float MINIMUM_DISTANCE_UPDATE_LOCATION = 1000;
+    public static final int REQUEST_CODE_GPS_LOCATION = 1000; // Código para peticiones GPS
+    public static final long MINIMUM_TIME_UPDATE_LOCATION = 60000; // Cada minuto se mira si hay actualizaciones GPS
+    public static final float MINIMUM_DISTANCE_UPDATE_LOCATION = 1000; // Mínimo de 1 km respecto posición anterior para actualizar coordenadas GPS
 
 
 }
